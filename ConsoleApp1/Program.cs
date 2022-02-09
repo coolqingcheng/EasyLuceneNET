3e0a9babc6947ade1f21a8cc6b9fa5ea40fe41c1
qingcheng <qingchengcode@qq.com>
2022-02-09 17:13:45 +0800

    update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using jieba.NET;
+using JiebaNet.Segmenter;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+
+const LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
+
+var indexPath = Path.Combine(AppContext.BaseDirectory, "indexs");
+
+using var dir = FSDirectory.Open(indexPath);
+
+// Create an analyzer to process the text
+var analyzer = new JieBaAnalyzer(TokenizerMode.Search);
+
+// Create an index writer
+var indexConfig = new IndexWriterConfig(AppLuceneVersion, analyzer);
+using var writer = new IndexWriter(dir, indexConfig);
+
+
+
+#region 创建索引
+
+for (int i = 0; i < 100; i++)
+{
+    var doc = new Document
+{
+    // StringField indexes but doesn't tokenize
+    new StringField("name",
+        "张三",
+        Field.Store.YES),
+    new TextField("desc",
+        "小明硕士毕业于中国科学院计算所，后在日本京都大学深造",
+        Field.Store.YES)
+};
+
+    writer.AddDocument(doc);
+    writer.Flush(triggerMerge: false, applyAllDeletes: false);
+}
+writer.Flush(triggerMerge: true, applyAllDeletes: false);
+writer.Commit();
+#endregion
+
+#region 查询
+
+// Search with a phrase
+var query = new BooleanQuery();
+query.Add(new TermQuery(new Term("desc", "小明")), Occur.SHOULD);
+
+using var reader = writer.GetReader(applyAllDeletes: true);
+var searcher = new IndexSearcher(reader);
+var hits = searcher.Search(query, 20 /* top 20 */).ScoreDocs;
+// Display the output in a table
+Console.WriteLine($"{"Score",10}" +
+    $" {"Name",-15}" +
+    $" {"Favorite Phrase",-40}");
+foreach (var hit in hits)
+{
+    var foundDoc = searcher.Doc(hit.Doc);
+    Console.WriteLine($"{hit.Score:f8}" +
+        $" {foundDoc.Get("name"),-15}" +
+        $" {foundDoc.Get("desc"),-40}");
+}
+
+
+
+#endregion
+
+
+
+
+var segmenter = new JiebaSegmenter();
+var s = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造";
+var tokens = segmenter.Tokenize(s, TokenizerMode.Search);
+
+Console.WriteLine("");
+
---
+++ jieba.NET/JieBaTokenizer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Lucene.Net.Analysis.TokenAttributes;
+using Lucene.Net.Analysis;
+using JiebaNet.Segmenter;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.FileProviders;
+
+namespace jieba.NET
+{
+    public class JieBaTokenizer
+        : Tokenizer
+    {
+        private static bool _initial = false;
+        private string _inputText;
+        private bool _originalResult = false;
+        private int _start = 0;
+
+        private readonly string _stropWordsPath = "Resources/stopwords.txt";
+
+        private readonly JiebaSegmenter _segmenter;
+        private TokenizerMode _mode;
+        private ICharTermAttribute _termAtt;
+        private IOffsetAttribute _offsetAtt;
+        private IPositionIncrementAttribute _posIncrAtt;
+        private ITypeAttribute _typeAtt;
+
+        private Dictionary<string, int> _stopWords = new Dictionary<string, int>();
+        private List<JiebaNet.Segmenter.Token> _wordList = new List<JiebaNet.Segmenter.Token>();
+
+        private IEnumerator<JiebaNet.Segmenter.Token> _iter;
+
+        public JieBaTokenizer(TextReader input, TokenizerMode Mode)
+            : base(AttributeFactory.DEFAULT_ATTRIBUTE_FACTORY, input)
+        {
+            _segmenter = new JiebaSegmenter();
+            _mode = Mode;
+            LoadStopWords();
+            Init();
+        }
+
+        public Dictionary<string, int> StopWords
+        {
+            get => _stopWords;
+        }
+
+        private void LoadStopWords()
+        {
+            var fileProvider = new EmbeddedFileProvider(GetType().GetTypeInfo().Assembly);
+            var fileInfo = fileProvider.GetFileInfo(_stropWordsPath);
+
+            using (var reader = new StreamReader(fileInfo.CreateReadStream()))
+            {
+                var s = "";
+                while ((s = reader.ReadLine()) != null)
+                {
+                    if (String.IsNullOrEmpty(s))
+                        continue;
+                    if (_stopWords.ContainsKey(s))
+                        continue;
+                    _stopWords.Add(s, 1);
+                }
+            }
+        }
+
+        private void Init()
+        {
+            _termAtt = AddAttribute<ICharTermAttribute>();
+            _offsetAtt = AddAttribute<IOffsetAttribute>();
+            _posIncrAtt = AddAttribute<IPositionIncrementAttribute>();
+            _typeAtt = AddAttribute<ITypeAttribute>();
+        }
+
+        private string ReadToEnd(TextReader input)
+        {
+            return input.ReadToEnd();
+        }
+
+        public sealed override Boolean IncrementToken()
+        {
+            ClearAttributes();
+
+            var word = Next();
+            if (word != null)
+            {
+                var buffer = word.ToString();
+                _termAtt.SetEmpty().Append(buffer);
+                _offsetAtt.SetOffset(CorrectOffset(word.StartOffset), CorrectOffset(word.EndOffset));
+                _typeAtt.Type = word.Type;
+                return true;
+            }
+
+            End();
+            Dispose();
+            return false;
+        }
+
+        private Lucene.Net.Analysis.Token Next()
+        {
+            var length = 0;
+            var res = _iter.MoveNext();
+            if (res)
+            {
+                var word = _iter.Current;
+                var token = new Lucene.Net.Analysis.Token(word.Word, word.StartIndex, word.EndIndex);
+                _start += length;
+                return token;
+            }
+            return null;
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            _inputText = ReadToEnd(base.m_input);
+            RemoveStopWords(_segmenter.Tokenize(_inputText, _mode));
+
+            _start = 0;
+            _iter = _wordList.GetEnumerator();
+        }
+
+        private void RemoveStopWords(IEnumerable<JiebaNet.Segmenter.Token> words)
+        {
+            _wordList.Clear();
+
+            foreach (var x in words)
+            {
+                if (!_stopWords.ContainsKey(x.Word))
+                {
+                    _wordList.Add(x);
+                }
+            }
+        }
+    }
+}
